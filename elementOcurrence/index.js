7b1abd8b55ce68199da9e03a070bc93e1e37e2a5
georginarojas <ginarojas29@gmail.com>
2022-08-16 15:26:19 -0300

    element occurrence

--- elementOcurrence/index.js
+++ elementOcurrence/index.js
@@ -2,17 +2,13 @@
     Find element occurrence in array
 */
 function occurrence(list) {
-    const count = {}
+  const count = {};
 
-    for(const el of list) {
-        console.log(`el ${el} count[el]: ${count[el]}`)
-        count[el] = count[el]? count[el] + 1 : 1
-        console.log('Count ', count)
-    }
-    console.log(count)
-    return count
+  for (const el of list) {
+    count[el] = count[el] ? count[el] + 1 : 1;
+  }
+  return count;
 }
 
-const list = [1,1,2,3,1,3]
-occurrence(list)
-
+const list = [1, 1, 2, 3, 1, 3];
+occurrence(list);
